6865a8816e0dde09d37fa5d2e4d515215a643613
wesley <wesbourkeavl@gmail.com>
2022-03-12 12:24:50 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Add your code here
+
+function submitData(name, email) {  
+    const nameingObject = {
+        name: name,
+        email: email,
+    }
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(nameingObject),
+      };
+
+  fetch("http://localhost:3000/users", configurationObject)
+  .then((response) => {
+    return response.json();
+  })
+  .then((object) => {
+    let idValue = object.id
+    const postLocation = document.querySelector('#here')
+    postLocation.append(idValue)
+  })
+  .catch((error) => {
+    //alert("Bad things! Ragnarők!");
+    let errorMess = error.message;
+    console.log(errorMess)
+    postLocation.append(errorMess)
+  });
+}
+//submitData("sad " , "dad")
